57d1f0e1af7530e58bc44af685cdd705c241807c
akky-xxxx <114.s.aki@gmail.com>
2022-04-29 19:22:31 +0900

    feat: use openApiValidator

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from "express"
+import { middleware as openApiValidator } from "express-openapi-validator"
 
 const server = express()
 
+server.use(
+  openApiValidator({
+    apiSpec: "./src/openapi.yml",
+    validateResponses: true,
+  })
+)
+
 server.get("/", (_req, res) => {
-  res.json("Hello express")
+  res.json({
+    message: "Hello express",
+    test: "1234-12-12",
+    test2: "2"
+  })
 })
 
 server.listen(3000, () => {
